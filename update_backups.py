77c90e2751d8d5aaedd0b07da8c30a436c3b1f10
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-02-17 09:43:11 -0500

    refactor sheets-API code

--- update_backups.py
+++ update_backups.py
@@ -57,24 +57,37 @@ def latest_timemachine_backup():
     return datetime.strptime(line.split('/')[-1], '%Y-%m-%d-%H%M%S')
 
 
-def main():
+spreadsheetId = '11X3adqmCRztAaNjY0dKbeyds7J_yu6HgkyyGtgNZR-0'
+
+
+def spreadsheets():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
                     'version=v4')
-    service = discovery.build('sheets', 'v4', http=http,
-                              discoveryServiceUrl=discoveryUrl)
 
-    spreadsheetId = '11X3adqmCRztAaNjY0dKbeyds7J_yu6HgkyyGtgNZR-0'
+    return discovery.build('sheets', 'v4',
+                           http=http,
+                           discoveryServiceUrl=discoveryUrl
+                           ).spreadsheets().values()
+
+
+def spreadsheet_values():
     rangeName = 'A1:D5'
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    result = spreadsheets().get(spreadsheetId=spreadsheetId,
+                                range=rangeName).execute()
+
     values = result.get('values', [])
     if not values:
         raise Exception('No data found.')
 
+    for rownum, (disk, data, where, copied) in enumerate(values):
+        yield rownum + 1, disk, data, where, copied
+
+
+def main():
     if flags.event == 'copied':
-        for rownum, (disk, data, where, copied) in enumerate(values):
+        for rownum, disk, data, where, copied in spreadsheet_values():
             if data.lower() == flags.data.lower() and where == "home":
                 print("%s last copied to \"%s\" on %s." % (
                     data.title(), disk, copied))
@@ -89,24 +102,24 @@ def main():
             latest_backup = datetime.now().strftime('%Y/%m/%d')
 
         body = {"values": [[latest_backup]]}
-        service.spreadsheets().values().update(
+        spreadsheets().update(
             spreadsheetId=spreadsheetId,
-            range="D%d" % (rownum + 1),
+            range="D%d" % rownum,
             valueInputOption="USER_ENTERED",
             body=body
-        ).execute()
+        )
 
         print("Updated: %s." % latest_backup)
     elif flags.event == "swapped":
         home_disk = None
         office_disk = None
-        for rownum, (disk, data, where, copied) in enumerate(values):
+        for rownum, disk, data, where, copied in spreadsheet_values():
             if data.lower() == flags.data.lower():
                 if where == "home":
                     home_disk = rownum
                 elif where == "office":
                     office_disk = rownum
-                    
+
         if not home_disk:
             raise Exception("Couldn't find home disk for \"%s\"", flags.data)
 
@@ -115,15 +128,15 @@ def main():
 
         # Swap home and office.
         data = [{
-            "range": "C%d" % (home_disk + 1),
+            "range": "C%d" % home_disk,
             "values": [["office"]]
         }, {
-            "range": "C%d" % (office_disk + 1),
+            "range": "C%d" % office_disk,
             "values": [["home"]]
         }]
 
         body = {"data": data, "valueInputOption": "USER_ENTERED"}
-        service.spreadsheets().values().batchUpdate(
+        spreadsheets().batchUpdate(
             spreadsheetId=spreadsheetId, body=body).execute()
     else:
         raise argparse.ArgumentError(
